e982f3c885a87b32c5ef96b48e84ebd1e36e4467
lifereze <liferezearnold@gmail.com>
2020-07-26 05:03:43 +0300

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -27,7 +27,6 @@ import { GitSearchService } from './git-search.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     HttpClientModule,
     RoutingModule,

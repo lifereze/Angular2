d5b8fb29ab12cca62c76e66b025d63c570be616b
lifereze <liferezearnold@gmail.com>
2020-07-26 04:07:09 +0300

    add logic functions to highlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(el: ElementRef) {
+    el.nativeElement.style.backgroundColor = 'green';
+     }
 
 }
